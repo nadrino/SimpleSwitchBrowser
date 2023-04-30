a12f1f5337acea99b0eed3e93f0173387d0dd7b7
Adrien Blanchet <adrien.blanchet@live.fr>
2023-04-30 14:09:13 +0200

    adding base cmake files

---
+++ env.sh
@@ -0,0 +1,25 @@
+#! /bin/bash
+
+function setup_devkitpro() {
+    echo "Seting up DevKitPro..." >&2
+    export DEVKITPRO=/opt/devkitpro
+    export DEVKITA64=${DEVKITPRO}/devkitA64
+    export DEVKITARM=${DEVKITPRO}/devkitARM
+    export DEVKITPPC=${DEVKITPRO}/devkitPPC
+    export PORTLIBS_PREFIX=${DEVKITPRO}/portlibs/switch
+
+    export PATH=${DEVKITPRO}/tools/bin:$PATH
+    export PATH=${DEVKITA64}/bin/:$PATH
+
+    return;
+}; export -f setup_devkitpro
+
+function send_file(){
+
+  HOST='192.168.1.75:5000'
+  lftp -e "cd $2; put $1; bye" $HOST
+
+}; export -f send_file
+
+
+setup_devkitpro
